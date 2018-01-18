05ea49cd4f9fa9cf65ef44df33e1fd889a3aaba6
Anthony Hinsinger <anthony.hinsinger@gmail.com>
2018-01-18 19:18:33 +0100

    Added loramac initialisation test

--- src/apps/test/NucleoL073RZ/main.c
+++ src/apps/test/NucleoL073RZ/main.c
@@ -1,4 +1,15 @@
 #include "board.h"
+#include "LoRaMac.h"
+#include "LoRaMacTest.h"
+
+#define IEEE_OUI                                    0x00, 0x00, 0x00
+#define LORAWAN_DEVICE_EUI                          { IEEE_OUI, 0xC8, 0x1E, 0xFE, 0xBA, 0xA7 }
+#define LORAWAN_APPLICATION_EUI                     { 0x70, 0xB3, 0xD5, 0x7E, 0xF0, 0x00, 0x40, 0x5D }
+#define LORAWAN_APPLICATION_KEY                     { 0x15, 0x8A, 0x34, 0x3B, 0xCC, 0x3E, 0xDE, 0xEC, 0xD2, 0x93, 0x45, 0xB4, 0xD3, 0x64, 0xA4, 0x37 }
+
+static uint8_t DevEui[] = LORAWAN_DEVICE_EUI;
+static uint8_t AppEui[] = LORAWAN_APPLICATION_EUI;
+static uint8_t AppKey[] = LORAWAN_APPLICATION_KEY;
 
 static TimerEvent_t Led1Timer;
 
@@ -12,24 +23,89 @@ void timerCb() {
     TimerStart(&Led1Timer);
 }
 
+static void McpsConfirm( McpsConfirm_t *mcpsConfirm )
+{
+}
+
+static void McpsIndication( McpsIndication_t *mcpsIndication )
+{
+}
+
+static void MlmeConfirm( MlmeConfirm_t *mlmeConfirm )
+{
+    switch( mlmeConfirm->MlmeRequest )
+    {
+        case MLME_JOIN:
+        {
+            TimerStop(&Led1Timer);
+            TimerSetValue(&Led1Timer, 100);
+            TimerStart(&Led1Timer);
+            return;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    TimerStop(&Led1Timer);
+    TimerSetValue(&Led1Timer, 1000);
+    TimerStart(&Led1Timer);
+}
+
+
+
 /**
  * Main application entry point.
  */
 int main( void )
 {
-        BoardInitMcu( );
-        BoardInitPeriph( );
+    LoRaMacPrimitives_t LoRaMacPrimitives;
+    LoRaMacCallback_t LoRaMacCallbacks;
+    MibRequestConfirm_t mibReq;
 
-        TimerInit(&Led1Timer, &timerCb);
-        TimerSetValue(&Led1Timer, GetBoardPowerSource() == BATTERY_POWER ? 100 : 500);
-        TimerStart(&Led1Timer);
+    BoardInitMcu( );
+    BoardInitPeriph( );
 
-        //GpioInit( &Button, NSWITCH_1, PIN_INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
-        //GpioSetInterrupt( &Button, IRQ_RISING_EDGE, IRQ_MEDIUM_PRIORITY, ( GpioIrqHandler * )timerCb);
+    LoRaMacPrimitives.MacMcpsConfirm = McpsConfirm;
+    LoRaMacPrimitives.MacMcpsIndication = McpsIndication;
+    LoRaMacPrimitives.MacMlmeConfirm = MlmeConfirm;
+    LoRaMacCallbacks.GetBatteryLevel = BoardGetBatteryLevel;
 
-        while(1) {
-            printf("bla\r\n");
-            //UartMcuPutChar( &Uart1, 'a' );
-            DelayMs(1000);
-        }
+    TimerInit(&Led1Timer, &timerCb);
+    TimerSetValue(&Led1Timer, GetBoardPowerSource() == BATTERY_POWER ? 100 : 500);
+    TimerStart(&Led1Timer);
+
+    LoRaMacInitialization( &LoRaMacPrimitives, &LoRaMacCallbacks, LORAMAC_REGION_EU868 );
+    mibReq.Type = MIB_ADR;
+    mibReq.Param.AdrEnable = true;
+    LoRaMacMibSetRequestConfirm( &mibReq );
+
+    mibReq.Type = MIB_PUBLIC_NETWORK;
+    mibReq.Param.EnablePublicNetwork = true;
+    LoRaMacMibSetRequestConfirm( &mibReq );
+
+    LoRaMacTestSetDutyCycleOn( false );
+
+    MlmeReq_t mlmeReq;
+
+    // Initialize LoRaMac device unique ID
+    BoardGetUniqueId( DevEui );
+
+    mlmeReq.Type = MLME_JOIN;
+
+    mlmeReq.Req.Join.DevEui = DevEui;
+    mlmeReq.Req.Join.AppEui = AppEui;
+    mlmeReq.Req.Join.AppKey = AppKey;
+    mlmeReq.Req.Join.NbTrials = 3;
+
+    LoRaMacMlmeRequest( &mlmeReq );
+
+    //GpioInit( &Button, NSWITCH_1, PIN_INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
+    //GpioSetInterrupt( &Button, IRQ_RISING_EDGE, IRQ_MEDIUM_PRIORITY, ( GpioIrqHandler * )timerCb);
+
+    while(1) {
+        printf("bla\r\n");
+        TimerLowPowerHandler( );
+    }
 }
--- src/boards/NucleoL073RZ/board.c
+++ src/boards/NucleoL073RZ/board.c
@@ -134,7 +134,7 @@ void BoardInitMcu( void )
 
         GpioInit( &ioPin, UART_RX, PIN_INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
 
-        GpioWrite(&Led1, GpioRead( &ioPin ));
+        //GpioWrite(&Led1, GpioRead( &ioPin ));
 
         if( GpioRead( &ioPin ) == 1 )   // Debug Mode
         {
@@ -162,8 +162,8 @@ void BoardInitMcu( void )
     //I2cInit( &I2c, I2C_SCL, I2C_SDA );
     //AdcInit( &Adc, BAT_LEVEL_PIN );
 
-    //SpiInit( &SX1272.Spi, RADIO_MOSI, RADIO_MISO, RADIO_SCLK, NC );
-    //SX1272IoInit( );
+    SpiInit( &SX1272.Spi, RADIO_MOSI, RADIO_MISO, RADIO_SCLK, NC );
+    SX1272IoInit( );
 
     if( McuInitialized == false )
     {
@@ -247,28 +247,6 @@ void SystemClockConfig( void )
     RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
     RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
 
-    /*RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
-    RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-    RCC_OscInitStruct.HSI48State = RCC_HSI48_OFF;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_12;
-    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_3;*/
-
-    /*RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = 16;
-    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
-    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;*/
     if( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK )
     {
         assert_param( FAIL );
@@ -285,9 +263,7 @@ void SystemClockConfig( void )
     }
 
     PeriphClkInit.PeriphClockSelection =  RCC_PERIPHCLK_USART2 | RCC_PERIPHCLK_RTC;
-    //PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
     PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-    //PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
     PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
     if( HAL_RCCEx_PeriphCLKConfig( &PeriphClkInit ) != HAL_OK )
     {
@@ -301,60 +277,6 @@ void SystemClockConfig( void )
     // SysTick_IRQn interrupt configuration
     HAL_NVIC_SetPriority( SysTick_IRQn, 0, 0 );
 }
-
-/*void SystemClockConfig( void )
-{
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-    RCC_PeriphCLKInitTypeDef PeriphClkInit;
-
-    __HAL_RCC_SYSCFG_CLK_ENABLE( );
-    __HAL_RCC_PWR_CLK_ENABLE( );
-
-//__PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG( PWR_REGULATOR_VOLTAGE_SCALE1 );
-
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = 16;
-    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
-    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
-    if( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK )
-    {
-        assert_param( FAIL );
-    }
-
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-    if( HAL_RCC_ClockConfig( &RCC_ClkInitStruct, FLASH_LATENCY_1 ) != HAL_OK )
-    {
-        assert_param( FAIL );
-    }
-
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_RTC;
-    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
-    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-    if( HAL_RCCEx_PeriphCLKConfig( &PeriphClkInit ) != HAL_OK )
-    {
-        assert_param( FAIL );
-    }
-
-    HAL_SYSTICK_Config( HAL_RCC_GetHCLKFreq( ) / 1000 );
-
-    HAL_SYSTICK_CLKSourceConfig( SYSTICK_CLKSOURCE_HCLK );
-
-    // SysTick_IRQn interrupt configuration
-    HAL_NVIC_SetPriority( SysTick_IRQn, 0, 0 );
-}*/
 
 void CalibrateSystemWakeupTime( void )
 {
